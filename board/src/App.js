953bd9e3d4f9be8a3a91385f999a10507e593093
kayraberktuncer <kayrabtuncer@gmail.com>
2021-06-03 23:44:25 +0300

    Prepared for the login screen.

--- board/src/App.js
+++ board/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
 
+import EntryPage from './components/EntryPage'
+
 function App() {
   return (
     <Container maxWidth="xl">
-      <div>Hello World</div>
+      <EntryPage />
     </Container>
   )
 }
