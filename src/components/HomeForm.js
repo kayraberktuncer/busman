84e00608afddc03161b673a911682976b4e387a3
kayraberktuncer <kayrabtuncer@gmail.com>
2021-09-09 21:17:57 +0300

    Add home form classes

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -5,52 +5,71 @@ export function HomeForm({ userData, setUserData }) {
   };
 
   return (
-    <div>
+    <div className="home-form">
       <form onSubmit={handleSubmit}>
-        <h1>Home Form</h1>
-        <input
-          type="text"
-          value={userData.companyName}
-          onChange={e =>
-            setUserData({ ...userData, companyName: e.target.value })
-          }
-          name="companyName"
-          id="companyName"
-          placeholder="Company Name"
-        />
-        <input
-          type="text"
-          value={userData.name}
-          onChange={e => setUserData({ ...userData, name: e.target.value })}
-          name="name"
-          id="name"
-          placeholder="Name"
-        />
-        <input
-          type="text"
-          value={userData.surname}
-          onChange={e => setUserData({ ...userData, surname: e.target.value })}
-          name="surname"
-          id="surname"
-          placeholder="Surname"
-        />
-        <input
-          type="text"
-          value={userData.country}
-          onChange={e => setUserData({ ...userData, country: e.target.value })}
-          name="country"
-          id="country"
-          placeholder="Country"
-        />
-        <input
-          type="text"
-          value={userData.slogan}
-          onChange={e => setUserData({ ...userData, slogan: e.target.value })}
-          name="slogan"
-          id="slogan"
-          placeholder="Slogan"
-        />
-        <button type="submit">Submit</button>
+        <div className="home-form-title">
+          <h1>Home Form</h1>
+        </div>
+        <div className="home-form-item">
+          <input
+            type="text"
+            value={userData.companyName}
+            onChange={e =>
+              setUserData({ ...userData, companyName: e.target.value })
+            }
+            name="companyName"
+            id="companyName"
+            placeholder="Company Name"
+          />
+        </div>
+        <div className="home-form-item">
+          <input
+            type="text"
+            value={userData.name}
+            onChange={e => setUserData({ ...userData, name: e.target.value })}
+            name="name"
+            id="name"
+            placeholder="Name"
+          />
+        </div>
+        <div className="home-form-item"></div>
+        <div className="home-form-item">
+          <input
+            type="text"
+            value={userData.surname}
+            onChange={e =>
+              setUserData({ ...userData, surname: e.target.value })
+            }
+            name="surname"
+            id="surname"
+            placeholder="Surname"
+          />
+        </div>
+        <div className="home-form-item">
+          <input
+            type="text"
+            value={userData.country}
+            onChange={e =>
+              setUserData({ ...userData, country: e.target.value })
+            }
+            name="country"
+            id="country"
+            placeholder="Country"
+          />
+        </div>
+        <div className="home-form-item">
+          <input
+            type="text"
+            value={userData.slogan}
+            onChange={e => setUserData({ ...userData, slogan: e.target.value })}
+            name="slogan"
+            id="slogan"
+            placeholder="Slogan"
+          />
+        </div>
+        <div className="home-form-item home-form-button">
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
